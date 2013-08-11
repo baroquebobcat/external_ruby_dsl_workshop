c4dc9c4f9e328fb0b7b7b8c840fbef6897fb54f3
Nick Howard <ndh@baroquebobcat.com>
2013-08-10 22:15:21 -0600

    indent tests

--- spec/sql_workshop_spec.rb
+++ spec/sql_workshop_spec.rb
@@ -2,22 +2,24 @@ require 'minitest/autorun'
 require 'sql_awesome'
 
 describe SQLAwesome do
-  it "has hello world, but with one" do
-    result = SQLAwesome.eval "SELECT 1"
+  describe "hello worldish" do
+    it "has hello world, but with one" do
+      result = SQLAwesome.eval "SELECT 1"
 
-    assert_equal [{"1"=>1}], result
-  end
+      assert_equal [{"1"=>1}], result
+    end
 
-  it "has hello world, but with 2" do
-    result = SQLAwesome.eval "SELECT 2"
-    
-    assert_equal [{"2"=>2}], result
-  end
+    it "has hello world, but with 2" do
+      result = SQLAwesome.eval "SELECT 2"
+      
+      assert_equal [{"2"=>2}], result
+    end
 
 
-  it "allows selecting multiple values" do
-    result = SQLAwesome.eval "SELECT 1,2,3"
-    
-    assert_equal [{"1"=>1},{"2"=>2},{"3"=>3}], result
+    it "allows selecting multiple values" do
+      result = SQLAwesome.eval "SELECT 1,2,3"
+      
+      assert_equal [{"1"=>1},{"2"=>2},{"3"=>3}], result
+    end
   end
 end
