407d091a321fad35a66059e819a0023cfcdf78a1
Nick Howard <ndh@baroquebobcat.com>
2013-09-11 21:03:19 -0600

    add test for multiple args in transform

--- spec/transformer_spec.rb
+++ spec/transformer_spec.rb
@@ -2,9 +2,14 @@ require 'minitest/autorun'
 require 'sql_awesome'
 
 describe SQLAwesome::Transformer do
-  it "converts {select_args: {integer: '1'}} into an Statement node" do
+  it "converts {select_args: [{integer: '1'}]} into an Statement node" do
     transformer = SQLAwesome::Transformer.new
     result = transformer.apply select_args: [{integer: "1"}]
     result.inspect.must_equal "Statement[1]"
+  end
+  it "converts {select_args: [{integer: '1'},{integer: '2'}]} into an Statement node" do
+    transformer = SQLAwesome::Transformer.new
+    result = transformer.apply select_args: [{integer: "1"},{integer: "2"}]
+    result.inspect.must_equal "Statement[1, 2]"
   end
 end
