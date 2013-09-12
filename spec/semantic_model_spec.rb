50055fddc1d9544b65c4e7804e749d3fa68e7cfa
Nick Howard <ndh@baroquebobcat.com>
2013-09-11 20:33:37 -0600

    add test I should have added earlier for semantic model

--- spec/semantic_model_spec.rb
+++ spec/semantic_model_spec.rb
@@ -12,4 +12,9 @@ describe SQLAwesome::SemanticModel do
     statement = Model::Statement.new [Model::Integer.new("2")]
     statement.eval(nil).must_equal [{"2" => 2 }]
   end
+
+  it "handles multiple arguments" do
+    statement = Model::Statement.new [Model::Integer.new("1"), Model::Integer.new("2")]
+    statement.eval(nil).must_equal [{"1" => 1, "2" => 2 }]
+  end
 end
