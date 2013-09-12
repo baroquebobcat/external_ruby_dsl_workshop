7833b69648ad9e42fc9dc794385ab6a0dfabd0d6
Nick Howard <ndh@baroquebobcat.com>
2013-09-11 20:29:37 -0600

    update the transformer to the new intermediate tree

--- lib/sql_awesome/transformer.rb
+++ lib/sql_awesome/transformer.rb
@@ -1,6 +1,6 @@
 module SQLAwesome
   class Transformer < Parslet::Transform
-    rule(:select_args => simple(:args))  { SemanticModel::Statement.new args }
+    rule(:select_args => sequence(:args))  { SemanticModel::Statement.new args }
     rule(:integer => simple(:int))  { SemanticModel::Integer.new int }
   end
 end
--- spec/transformer_spec.rb
+++ spec/transformer_spec.rb
@@ -4,7 +4,7 @@ require 'sql_awesome'
 describe SQLAwesome::Transformer do
   it "converts {select_args: {integer: '1'}} into an Statement node" do
     transformer = SQLAwesome::Transformer.new
-    result = transformer.apply select_args: { integer: "1" }
+    result = transformer.apply select_args: [{integer: "1"}]
     result.inspect.must_equal "Statement[1]"
   end
 end
