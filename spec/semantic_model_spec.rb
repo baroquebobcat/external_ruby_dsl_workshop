dc513145cbc7a2e7dc1c7d1f1a339111a087ad5a
Nick Howard <ndh@baroquebobcat.com>
2013-09-11 20:59:59 -0600

    nest by no table for semantic model

--- spec/semantic_model_spec.rb
+++ spec/semantic_model_spec.rb
@@ -3,18 +3,20 @@ require 'sql_awesome'
 
 Model = SQLAwesome::SemanticModel
 describe SQLAwesome::SemanticModel do
-  it "hello worlds with one" do
-    statement = Model::Statement.new [Model::Integer.new("1")]
-    statement.eval(nil).must_equal [{"1"=>1}]
-  end
+  describe "no tables" do
+    it "hello worlds with one" do
+      statement = Model::Statement.new [Model::Integer.new("1")]
+      statement.eval(nil).must_equal [{"1"=>1}]
+    end
 
-  it "has hello world, but with 2" do
-    statement = Model::Statement.new [Model::Integer.new("2")]
-    statement.eval(nil).must_equal [{"2" => 2 }]
-  end
+    it "has hello world, but with 2" do
+      statement = Model::Statement.new [Model::Integer.new("2")]
+      statement.eval(nil).must_equal [{"2" => 2 }]
+    end
 
-  it "handles multiple arguments" do
-    statement = Model::Statement.new [Model::Integer.new("1"), Model::Integer.new("2")]
-    statement.eval(nil).must_equal [{"1" => 1, "2" => 2 }]
+    it "handles multiple arguments" do
+      statement = Model::Statement.new [Model::Integer.new("1"), Model::Integer.new("2")]
+      statement.eval(nil).must_equal [{"1" => 1, "2" => 2 }]
+    end
   end
 end
