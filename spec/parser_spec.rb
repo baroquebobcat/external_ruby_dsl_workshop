f0b310a5d6d56c80eac215b0c74b77082e28d6d0
Nick Howard <ndh@baroquebobcat.com>
2013-09-11 19:50:07 -0600

    add test showing we want another layer of hierarchy in the arg list

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -5,6 +5,6 @@ describe SQLAwesome::Parser do
   it "can parse hello world into intermediate tree" do
     parser = SQLAwesome::Parser.new
     tree = parser.parse "SELECT 1"
-    tree.must_equal integer: "1"
+    tree.must_equal select_args: {integer: "1"}
   end
 end
