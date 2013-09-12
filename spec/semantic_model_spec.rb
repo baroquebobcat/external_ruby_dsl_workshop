3aa3ec7768a65a64404cd81af08db52140fe759a
Nick Howard <ndh@baroquebobcat.com>
2013-09-11 19:46:30 -0600

    makes more sense for the 2 test in the semantic model

--- spec/semantic_model_spec.rb
+++ spec/semantic_model_spec.rb
@@ -2,8 +2,13 @@ require 'minitest/autorun'
 require 'sql_awesome'
 
 describe SQLAwesome::SemanticModel do
-  it "hello worlds" do
-    statement = SQLAwesome::SemanticModel::Statement.new 1
+  it "hello worlds with one" do
+    statement = SQLAwesome::SemanticModel::Statement.new "1"
     statement.eval(nil).must_equal [{"1"=>1}]
+  end
+
+  it "has hello world, but with 2" do
+    statement = SQLAwesome::SemanticModel::Statement.new "2"
+    statement.eval(nil).must_equal [{"2" => 2 }]
   end
 end
--- spec/sql_workshop_spec.rb
+++ spec/sql_workshop_spec.rb
@@ -3,15 +3,10 @@ require 'sql_awesome'
 
 describe SQLAwesome do
   describe "hello worldish" do
-    it "has hello world, but with one" do
+    it "has hello world with one" do
       result = SQLAwesome.eval "SELECT 1"
 
       assert_equal [{"1"=>1}], result
-    end
-
-    it "has hello world, but with 2" do
-      result = SQLAwesome.eval "SELECT 2"
-      assert_equal [{"2"=>2}], result
     end
 
 
