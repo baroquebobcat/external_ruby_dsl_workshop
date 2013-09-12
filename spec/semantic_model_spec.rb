ab2359a745738fb9cdabee63c5c6d287c22b5a0e
Nick Howard <ndh@baroquebobcat.com>
2013-09-11 20:32:34 -0600

    add const to semantic model spec to make it less annoying

--- spec/semantic_model_spec.rb
+++ spec/semantic_model_spec.rb
@@ -1,14 +1,15 @@
 require 'minitest/autorun'
 require 'sql_awesome'
 
+Model = SQLAwesome::SemanticModel
 describe SQLAwesome::SemanticModel do
   it "hello worlds with one" do
-    statement = SQLAwesome::SemanticModel::Statement.new [SQLAwesome::SemanticModel::Integer.new("1")]
+    statement = Model::Statement.new [Model::Integer.new("1")]
     statement.eval(nil).must_equal [{"1"=>1}]
   end
 
   it "has hello world, but with 2" do
-    statement = SQLAwesome::SemanticModel::Statement.new [SQLAwesome::SemanticModel::Integer.new("2")]
+    statement = Model::Statement.new [Model::Integer.new("2")]
     statement.eval(nil).must_equal [{"2" => 2 }]
   end
 end
