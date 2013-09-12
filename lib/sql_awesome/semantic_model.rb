8d1f261bc876d0f8949f23b117bb3b9b30095a7f
Nick Howard <ndh@baroquebobcat.com>
2013-09-11 20:30:14 -0600

    break out int semantic model class & use it

--- lib/sql_awesome/semantic_model.rb
+++ lib/sql_awesome/semantic_model.rb
@@ -1,9 +1,24 @@
 module SQLAwesome
   module SemanticModel
-    class Statement < Struct.new(:int)
-      def eval(database); [{int.to_s => int.to_i}]; end
+    class Statement < Struct.new(:args)
+      def eval(database)
+        [
+          Hash[args.map{|a|[a.name, a.value]}]
+        ]
+      end
       def inspect
-        "Statement[#{int}]"
+        "Statement[#{args.map(&:inspect).join(", ")}]"
+      end
+    end
+    class Integer < Struct.new(:int)
+      def name
+        int.to_s
+      end
+      def value
+        int.to_i
+      end
+      def inspect
+        name
       end
     end
   end
--- spec/semantic_model_spec.rb
+++ spec/semantic_model_spec.rb
@@ -3,12 +3,12 @@ require 'sql_awesome'
 
 describe SQLAwesome::SemanticModel do
   it "hello worlds with one" do
-    statement = SQLAwesome::SemanticModel::Statement.new "1"
+    statement = SQLAwesome::SemanticModel::Statement.new [SQLAwesome::SemanticModel::Integer.new("1")]
     statement.eval(nil).must_equal [{"1"=>1}]
   end
 
   it "has hello world, but with 2" do
-    statement = SQLAwesome::SemanticModel::Statement.new "2"
+    statement = SQLAwesome::SemanticModel::Statement.new [SQLAwesome::SemanticModel::Integer.new("2")]
     statement.eval(nil).must_equal [{"2" => 2 }]
   end
 end
